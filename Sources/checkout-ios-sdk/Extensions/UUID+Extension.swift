ba12156cecdca89e23f43f82499d97e50aca94b7
Elikem Savie <elikem@menaget.com>
2022-11-02 08:38:00 +0000

    Add UUID version 1 generation

--- Sources/checkout-ios-sdk/Extensions/UUID+Extension.swift
+++ Sources/checkout-ios-sdk/Extensions/UUID+Extension.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+extension UUID {
+
+    var version1UUID: String {
+        var uuid: uuid_t = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        withUnsafeMutablePointer(to: &uuid) {
+            $0.withMemoryRebound(to: UInt8.self, capacity: 16) {
+                uuid_generate_time($0)
+            }
+        }
+
+        let finalUUID = UUID(uuid: uuid)
+        return finalUUID.uuidString
+    }
+
+}
