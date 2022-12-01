6bc86efc1a900e6a6687b8a922a9200b9ba3f843
Elikem Savie <elikem@menaget.com>
2022-12-01 11:10:51 +0000

    Fix issue of 'package missing'

--- Package.swift
+++ Package.swift
@@ -14,26 +14,22 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "checkout-ios-sdk",
-            targets: ["core-SDK"]),
+            targets: ["checkout-ios-sdk"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apollographql/apollo-ios.git", .upToNextMinor(from: "1.0.5")),
 //         .package(url: "https://github.com/realm/SwiftLint", branch: "main"),
-//         .package(name: "SchemaPackage", path: "./SchemaPackage")
-
-//         .product(name: "ApolloCodegenLib", package: "apollo-ios")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "core-SDK",
+            name: "checkout-ios-sdk",
             dependencies: [
                 .product(name: "Apollo", package: "apollo-ios"),
-//                .product(name: "SchemaPackage", package: "SchemaPackage")
             ],
-            path: "Sources",
+//            path: "",
             resources: [
 //                .copy("AuthorizePayment.graphql"),
 //                .copy("Resources/countrylistdata.json"),
