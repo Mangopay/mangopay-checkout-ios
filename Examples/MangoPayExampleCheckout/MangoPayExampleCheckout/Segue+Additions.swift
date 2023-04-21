715b87d7f91f337e3765632dd98aed676aef17e7
Elikem Savie <elikem@menaget.com>
2023-04-21 07:24:45 +0000

    Add segue logic

--- Examples/MangoPayExampleCheckout/MangoPayExampleCheckout/Segue+Additions.swift
+++ Examples/MangoPayExampleCheckout/MangoPayExampleCheckout/Segue+Additions.swift
@@ -5,4 +5,21 @@
 //  Created by Elikem Savie on 20/04/2023.
 //
 
-import Foundation
+import UIKit
+
+protocol SegueHandlerType {
+    associatedtype SegueIdentifier: RawRepresentable
+}
+
+extension SegueHandlerType where Self: UIViewController, SegueIdentifier.RawValue == String {
+    func performSegueWithIdentifier(segueIdentifier: SegueIdentifier, sender: Any?) {
+        performSegue(withIdentifier: segueIdentifier.rawValue, sender: sender)
+    }
+
+    func segueIdentifierForSegue(segue: UIStoryboardSegue) -> SegueIdentifier {
+        guard let identifier = segue.identifier, let segueIdentifier = SegueIdentifier(rawValue: identifier) else {
+            fatalError("Invalid segue identifier \(segue.identifier ?? "")")
+        }
+        return segueIdentifier
+    }
+}
