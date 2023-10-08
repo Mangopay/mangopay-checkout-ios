77059488fb03ee04ddc062e6b3d9585b283207b4
Elikem Savie <ext-elikem.savie@mangopay.com>
2023-10-08 13:06:41 +0000

    use t3 env for 3DS testing

--- Examples/MangopayDemo/MangopayDemo/AppDelegate.swift
+++ Examples/MangopayDemo/MangopayDemo/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
-        MangoPayCoreiOS.initialize(clientId: "checkoutsquatest", environment: .sandbox)
+        MangopayCoreiOS.initialize(clientId: "12345", environment: .t3)
         return true
     }
 
