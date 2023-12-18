06eff218943ff5c0a99829e83a0d4c0fe49836ff
Elikem Savie <ext-elikem.savie@mangopay.com>
2023-12-18 11:29:28 +0000

    Add paypal button and make some logic refactoring

---
+++ MangopayCheckoutSDK/Paypal/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Elikem Savie on 14/12/2023.
+//
+
+import Foundation
---
+++ MangopayCheckoutSDK/Utils/WebView/MGPWebView.swift
@@ -0,0 +1,20 @@
+//
+//  MGPWebView.swift
+//  
+//
+//  Created by Elikem Savie on 13/12/2023.
+//
+
+import UIKit
+
+class MGPWebView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
