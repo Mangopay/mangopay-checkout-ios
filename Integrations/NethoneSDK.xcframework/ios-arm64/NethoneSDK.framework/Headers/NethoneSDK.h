d4a539e815e47c413242ac42002be1f7d010af61
Marek Truszkowski <marek.truszkowski@mangopay.com>
2024-10-08 15:29:32 +0200

    NethoneSDK via spm
    NethoneSDK removed
    
    NethoneSDK via spm integration

--- Integrations/NethoneSDK.xcframework/ios-arm64/NethoneSDK.framework/Headers/NethoneSDK.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-FOUNDATION_EXPORT double NethoneSDKVersionNumber;
-FOUNDATION_EXPORT const unsigned char NethoneSDKVersionString[];
-
-#import <NethoneSDK/NTHNethone.h>
-#import <NethoneSDK/NTHApiError.h>
-#import <NethoneSDK/NTHTextFieldRegisterMode.h>
-
